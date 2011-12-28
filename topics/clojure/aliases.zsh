76de3d60b98742eaecdb910b2f36feed9bf435b7
Ray Leyva <me@raymleyva.com>
2011-12-28 12:11:29 -0800

    Added my .localrc, please make sure it doesn't clobber yours.

--- topics/clojure/aliases.zsh
+++ topics/clojure/aliases.zsh
@@ -1,2 +1,6 @@
-[[ -d "${CLOJURE_PATH}" ]] && alias clojure='java -cp ${JLINE_PATH}:${CLOJURE_PATH} jline.ConsoleRunner clojure.lang.Repl'
+# java -cp clojure.jar clojure.main -i init.clj -r args...
+# [[ -d "${CLOJURE_PATH}" ]] && alias clojure='java -cp ${JLINE_PATH}:${CLOJURE_PATH} jline.ConsoleRunner clojure.lang.Repl'
+#
+# Not working as expected ... so nuking it for now.
+# [[ -e "${CLOJURE_JAR}" && -e "${JLINE_JAR}" ]] && alias clojure='java -cp ${JLINE_JAR}:${CLOJURE_JAR} jline.ConsoleRunner clojure.main'
 
--- topics/ctags/aliases.zsh
+++ topics/ctags/aliases.zsh
@@ -1,1 +1,2 @@
-alias tag="/usr/local/bin/ctags -f '.tags' --exclude='build' --extra=+f -R ."
+alias tag="/usr/local/bin/ctags -o .tags \`find . | grep .py$\`"
+#alias tag="/usr/local/bin/ctags -f '.tags' -R ." # --exclude='build' --extra=+f -R ."
